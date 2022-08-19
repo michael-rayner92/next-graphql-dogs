f70d856ee9ac35f407cb28b60e1fd542988b3cc1
michael-rayner92 <39574445+michael-rayner92@users.noreply.github.com>
2022-08-19 15:02:42 +0200

    feat: :tada: tutorial completed

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+	reactStrictMode: true,
+	swcMinify: true,
+	webpack: (config) => {
+		if (!config.experiments) {
+			config.experiments = {};
+		}
+		config.experiments.topLevelAwait = true;
+		return config;
+	}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
